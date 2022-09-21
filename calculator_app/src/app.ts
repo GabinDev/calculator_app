3f631d62b16ea20e97468f3d735912bb88bc8a51
Gabin Gaby <gabindjomo21@gmail.com>
2022-09-21 23:44:42 +0100

    second commit

--- calculator_app/src/app.ts
+++ calculator_app/src/app.ts
@@ -4,8 +4,6 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 import auth from './middleware/auth';
-import { join } from 'path';
-import staticFiles from './utils/staticFiles';
 
 const app = express();
 
@@ -15,13 +13,10 @@ app.use(helmet.contentSecurityPolicy());
 app.use(helmet.hidePoweredBy());
 app.use(cors());
 app.use(morgan('combined'));
-
-app.use('/api/v1', apiV1);
 // app.use(auth)
 
-app.use(express.static(join(__dirname, 'src/public')))
-app.get('*', staticFiles)
 
+app.use('/api/v1', apiV1);
 
 
 
